88233a9f53b9c1f6446e846f1663c666cf6364cb
Avidor Turkewitz <avi.turkewitz@gmail.com>
2019-05-15 15:02:38 -0400

    add parser to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,4 @@
 FROM ruby:2.5
+
+COPY ./parser.rb /usr/local/bin/parser.rb
+RUN chmod +x /usr/local/bin/parser.rb
---
+++ parser.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+require 'json'
+
+def parse(cell)
+  original_code_array = cell["source"]
+
+  memo = {start: nil, end: nil}
+  original_code_array.each_with_index do |line, index|
+    memo[:start] = index if /#BEGIN/ =~ line
+    memo[:end] = index if /#END/ =~ line
+  end
+
+  indent = original_code_array[memo[:start] + 1].match(/^\s+/)
+  insert = indent[0] + "pass"
+  original_code_array.slice!(memo[:start], memo[:end])
+
+  cell["source"] = original_code_array.insert(memo[:start], insert)
+  cell
+end
+
+filename = "./index.ipynb"
+file = File.open filename
+json = JSON.load file
+
+new_json = json["cells"].map do |cell|
+  if cell["cell_type"] == "code"
+    parse(cell)
+  else
+    cell
+  end
+end
+
+puts new_json
